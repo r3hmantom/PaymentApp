a5905fcf650f8ddbedcd7fd4df695aeec6cf1b8c
Abdul Rehman <r3hmantom@gmail.com>
2023-04-11 19:25:44 +0500

    animated arrow

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,18 +3,18 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 const GetStarted = () => (
   <div
-    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    className={`${styles.flexCenter} group w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
   >
     <div
       className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full`}
     >
-      <div className={`${styles.flexStart} flex-row`}>
+      <div className={`${styles.flexStart} flex-row `}>
         <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
           <span className="text-gradient">Get</span>
         </p>
         <img
           src={arrowUp}
-          className="w-[23px] h-[23px] object-contain"
+          className="w-[23px] h-[23px] object-contain group-hover:-translate-y-3 origin-bottom duration-200 group-hover:rotate-[100deg]"
           alt="arrow"
         />
       </div>
