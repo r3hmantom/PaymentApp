b47ba8b5dcbba22d0242ed28125fc14b8cb9baff
Abdul Rehman <r3hmantom@gmail.com>
2023-04-11 19:47:35 +0500

    get started responsive

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,13 +3,13 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 const GetStarted = () => (
   <div
-    className={`${styles.flexCenter} group w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    className={`${styles.flexCenter} group w-[100px] h-[100px] sm:h-[140px] sm:w-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
   >
     <div
       className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full`}
     >
       <div className={`${styles.flexStart} flex-row `}>
-        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+        <p className="font-poppins font-medium text-[15px] sm:text-[18px] leading-[23px] mr-2">
           <span className="text-gradient">Get</span>
         </p>
         <img
@@ -18,7 +18,7 @@ const GetStarted = () => (
           alt="arrow"
         />
       </div>
-      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+      <p className="font-poppins font-medium text-[15px] sm:text-[18px] leading-[23px]">
         <span className="text-gradient">Started</span>
       </p>
     </div>
