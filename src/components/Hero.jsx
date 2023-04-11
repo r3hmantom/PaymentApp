1d437a4debfef7b048cf04a373300e57ee572e5c
Abdul Rehman <r3hmantom@gmail.com>
2023-04-11 19:30:53 +0500

    hero image responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,11 @@ const Hero = () => (
 
     {/* RIGHT SIDE  */}
     <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-      <img src={robot} alt="billing" className="w-full h-full relative z-[5]" />
+      <img
+        src={robot}
+        alt="billing"
+        className="w-1/2 md:w-full h-1/2 md:h-full relative z-[5]"
+      />
       <div className="absolute z-0 w-[40%] h-[35%] top-0 pink__gradient" />
       <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
       <div className="absolute z-0 w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
