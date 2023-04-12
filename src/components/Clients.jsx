379309e78fdb1d4ae1fbd1978ca72205c204be61
Abdul Rehman <r3hmantom@gmail.com>
2023-04-12 20:14:34 +0500

    clients framer

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,26 +1,64 @@
 import React from "react";
 import { clients } from "../constants";
 import styles from "../style";
+import { motion } from "framer-motion";
+const ClientSection = () => {
+  const cardVariantTop = {
+    offscreen: {
+      y: "20vh",
+      display: "none",
+    },
+    onscreen: {
+      y: 0,
+      display: "flex",
+      opacity: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.6,
+      },
+    },
+  };
+  // {
+  //   ...cardVariantTop,
+  //   onscreen: {
+  //     ...cardVariantTop.onscreen,
+  //     transition: { delay: index * 0.1 },
+  //   },
+  // }
 
-const ClientSection = () => (
-  <section className={`${styles.flexCenter} mt-4 mb-28 `}>
-    <div
-      className={`${styles.flexCenter}  flex-col gap-10 md:gap-1 md:flex-row w-full  `}
+  return (
+    <motion.section
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: false, amount: 0 }}
+      className={`${styles.flexCenter} mt-4 mb-28 `}
     >
-      {clients.map((client) => (
-        <div
-          key={client.id}
-          className={`${styles.flexCenter} flex-1 min-w-[120px] sm:min-w-[192px]`}
-        >
-          <img
-            src={client.logo}
-            alt="client"
-            className="w-[150px] md:w-[192px] text-white object-contain cursor-pointer transition-all duration-200 hover:brightness-[3] hover:scale-110 hover:-translate-y-1"
-          />
-        </div>
-      ))}
-    </div>
-  </section>
-);
+      <div
+        className={`${styles.flexCenter}  flex-col gap-10 md:gap-1 md:flex-row w-full  `}
+      >
+        {clients.map((client, index) => (
+          <motion.div
+            variants={{
+              ...cardVariantTop,
+              onscreen: {
+                ...cardVariantTop.onscreen,
+                transition: { duration: index * 0.1 + 0.1 },
+              },
+            }}
+            key={client.id}
+            className={`${styles.flexCenter} flex-1 min-w-[120px] sm:min-w-[192px]`}
+          >
+            <img
+              src={client.logo}
+              alt="client"
+              className="w-[150px] md:w-[192px] text-white object-contain cursor-pointer transition-all duration-200 hover:brightness-[3] hover:scale-110 hover:-translate-y-1"
+            />
+          </motion.div>
+        ))}
+      </div>
+    </motion.section>
+  );
+};
 
 export default ClientSection;
